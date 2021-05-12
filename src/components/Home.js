4d1095810b66c59ca3739e0be451afe0a7d7e6b2
Ghaffari Khaled <med.khaledghaffarii@gmail.com>
2021-05-12 15:42:00 +0100

    style: formate and change  backgroud home in HOme component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,28 @@
 import React from 'react'
-
+import styled from "styled-components";
 function Home() {
     return (
-        <div>
+        <Container>
             Hello Home
-        </div>
+        </Container>
     )
 }
 
 export default Home
+const Container = styled.div`
+  position: relative;
+  min-height: calc(100vh - 250px);
+  overflow-x: hidden;
+  display: block;
+  top: 72px;
+  padding: 0 calc(3.5vw + 5px);
+  &:after {
+    background: url("/images/home-background.png") center center / cover
+      no-repeat fixed;
+    content: "";
+    position: absolute;
+    inset: 0px;
+    opacity: 1;
+    z-index: -1;
+  }
+`;
