f3556924b6ef7a50a3d9b4cf634d7718fb30e33e
Ghaffari Khaled <med.khaledghaffarii@gmail.com>
2021-05-12 13:05:28 +0100

    refactor: add a nav menus in a Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,39 +1,43 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from "react";
+import styled from "styled-components";
 function Header() {
-return (
-  <Nav>
-    <Logo src="/images/logo.svg" />
-    <NavMenu>
-      <a>
-        <img src="/images/home-icon.svg" />
-        <span>HOME</span>
-      </a>
-      <a>
-        <img src="/images/search-icon.svg" />
-        <span>SEARCH</span>
-      </a>
-      <a>
-        <img src="/images/watchlist-icon.svg" />
-        <span>WATCHLIST</span>
-      </a>
-      <a>
-        <img src="/images/original-icon.svg" />
-        <span>ORIGINAL</span>
-      </a>
-      <a>
-        <img src="/images/movie-icon.svg" />
-        <span>MOVIES</span>
-      </a>
-      <a>
-        <img src="/images/series-icon.svg" />
-        <span>SERIES</span>
-      </a>
-    </NavMenu>
-  </Nav>
-);
+  return (
+    <Nav>
+      <Logo src="/images/logo.svg" />
+      <NavMenu>
+        <a>
+          <img src="/images/home-icon.svg" />
+          <span>HOME</span>
+        </a>
+        <a>
+          <img src="/images/search-icon.svg" />
+          <span>SEARCH</span>
+        </a>
+        <a>
+          <img src="/images/watchlist-icon.svg" />
+          <span>WATCHLIST</span>
+        </a>
+        <a>
+          <img src="/images/original-icon.svg" />
+          <span>ORIGINAL</span>
+        </a>
+        <a>
+          <img src="/images/movie-icon.svg" />
+          <span>MOVIES</span>
+        </a>
+        <a>
+          <img src="/images/series-icon.svg" />
+          <span>SERIES</span>
+        </a>
+        <User>
+        
+            <img src="https://avatars.githubusercontent.com/u/75438046?s=400&u=4ad31515204e1d0d63e7940b107a25b85382a77c&v=4" />
+         
+        </User>
+      </NavMenu>
+    </Nav>
+  );
 }
-
 
 export default Header;
 
