ff87f23ce6373236e37c262e11621c926ba02696
Ghaffari Khaled <med.khaledghaffarii@gmail.com>
2021-05-15 21:27:34 +0100

    refactor: add a description box in Login Component

--- src/components/Login.js
+++ src/components/Login.js
@@ -6,7 +6,12 @@ function Login() {
         <Content>
           <CTA>
             <CTALogoOne src="/images/cta-logo-one.svg" />
-          <SignUp> GET ALL THREE </SignUp>
+            <SignUp> GET ALL THREE</SignUp>
+            <Description>
+              Get Premier Access to Raya and the Last Dragon for an additional
+              fee with a Disney+ subscription. As of 03/26/21, the price of
+              Disney+ and The Disney Bundle will increase by $1.
+            </Description>
           </CTA>
         </Content>
       </Container>
