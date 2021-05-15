8142086a5814abf92452ccddc5978ae961a91add
Ghaffari Khaled <med.khaledghaffarii@gmail.com>
2021-05-15 13:12:53 +0100

    refactor: define the routerswith used react-router  in App.js component

--- src/App.js
+++ src/App.js
@@ -4,11 +4,23 @@ import { Counter } from './features/counter/Counter';
 import './App.css';
 import Header from "./components/Header";
 import Home from "./components/Home";
+import Details from "./components/Details";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Home />
+      <Router>
+        <Header />
+        <Switch>
+          <Route path="/detail">
+            <Details />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
+      
     </div>
   );
 }
