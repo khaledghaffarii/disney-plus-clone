f8c2578f2b66040d23b5bbd547cb45bf92838e71
Ghaffari Khaled <med.khaledghaffarii@gmail.com>
2021-05-15 20:49:10 +0100

    style: add Login background and formate style container In Login component

---
+++ src/components/Login.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import styled from "styled-components";
+function Login() {
+    return (
+        <Container>
+            hello Login
+        </Container>
+    )
+}
+
+export default Login
+
+const Container = styled.section`
+  overflow: hidden;
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+
+  &:before {
+    height: 100%;
+    background-position: top;
+    background-size: cover;
+    background-repeat: no-repeat;
+    background: url("/images/login-background.jpg") center center / cover
+      no-repeat fixed;
+    content: "";
+    position: absolute;
+    inset: 0px;
+    opacity: 1;
+    z-index: -1;
+    top: 0;
+    right: 0;
+    left: 0;
+    
+  }
+`;
