67c2a3be78f01c525f578e4658e0155d5ca4ce53
Ghaffari Khaled <med.khaledghaffarii@gmail.com>
2021-07-16 12:53:21 +0100

    refactor: change firebase config for intersept movies data in firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,23 @@
 import firebase from 'firebase';
 
+// const firebaseConfig = {
+//   apiKey: "AIzaSyAks4j872WaPRqM2YlmsCgdwdhzBfkDiW4",
+//   authDomain: "disney-clone-97e30.firebaseapp.com",
+//   projectId: "disney-clone-97e30",
+//   storageBucket: "disney-clone-97e30.appspot.com",
+//   messagingSenderId: "567609214507",
+//   appId: "1:567609214507:web:83c149b0a1ace6fbcbcc6e",
+//   measurementId: "G-EWJSNNXHG3",
+// }; 
 const firebaseConfig = {
-  apiKey: "AIzaSyAlS902Ik4pCbuiyLrMWQCNAuPpqvu_bsc",
-  authDomain: "disneyplus-clone-3389f.firebaseapp.com",
-  projectId: "disneyplus-clone-3389f",
-  storageBucket: "disneyplus-clone-3389f.appspot.com",
-  messagingSenderId: "29844212960",
-  appId: "1:29844212960:web:60202dd699900b0b965485",
-  measurementId: "G-FVCJQ729VQ",
+  apiKey: "AIzaSyA9BnlX96fMf7XiUVCFRsoQzG8DGERJkeY",
+  authDomain: "disneyplus-clone-a33d5.firebaseapp.com",
+  projectId: "disneyplus-clone-a33d5",
+  storageBucket: "disneyplus-clone-a33d5.appspot.com",
+  messagingSenderId: "37918794208",
+  appId: "1:37918794208:web:dbe9842dfe1dda522a4b85",
+  measurementId: "G-DRVLJKWRWG",
 };
-
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 const db = firebaseApp.firestore();
 const auth = firebase.auth();
